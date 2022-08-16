aa41126c65b1517842577dbc1077ed3667fb1231
Elu Gonzalez <elugon10@gmail.com>
2022-08-16 21:11:15 +0200

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Counter from './components/Counter';
 function App() {
 
   const [showCounter, setShowCounter] = useState(true)
+  
 
   return (
     <div className="App">
       <h1>Hello world</h1>
       {showCounter && <Counter />}
+      <button onClick={() => setShowCounter(prev => !prev)}>{showCounter ? 'Hide' : 'Show'}</button>
     </div>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -7,15 +7,39 @@ export default function Counter() {
 
   // First iteration
 
+  const handleIncrese= () => setCount(prev=>prev + 1)
+  const handleDecrese= () => setCount(prev=>prev - 1)
+
   // Second and Fourth iteration
+
+  useEffect(()=>{
+    setCount(Math.floor(Math.random() * 10))
+    return ()=>{
+      setCount(0)
+      setDynamicClass('text')
+      console.log('All clean here. Count:', count, 'Class:', dynamicClass);
+    }
+  },[])
+
+  useEffect(()=>{
+
+    if(count<=0){
+      setDynamicClass('red')
+    } else if(count>1 && count<6){
+      setDynamicClass('orange')
+    } else{
+      setDynamicClass('green')
+    }
+
+  },[count])
 
   // Third iteration
 
   return (
     <div>
       <p>Counter: <span className={dynamicClass}>{count}</span></p>
-      <button>+ Increase</button>
-      <button>- Decrease</button>
+      <button onClick={handleIncrese}>+ Increase</button>
+      <button onClick={handleDecrese}>- Decrease</button>
     </div>
   )
 }
